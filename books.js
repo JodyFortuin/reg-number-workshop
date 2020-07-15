37cc4bc9a58971c33f7f44db736626d920caecd6
Jody <jodifortuin@gmail.com>
2020-07-15 14:53:29 +0200

    commit

--- books.js
+++ books.js
@@ -3,3 +3,14 @@ const bookTemplateElem = document.querySelector(".bookTemplate");
 const message1 = document.querySelector(".message1");
 const message2 = document.querySelector(".message2");
 
+console.log(bookTemplateElem.innerHTML);
+
+const booksTemplate = Handlebars.compile(bookTemplateElem.innerHTML);
+
+
+message1.innerHTML =  booksTemplate({
+   firstName: "Superman",
+    bookCount: 33,
+    dayCount: 300}
+
+);
